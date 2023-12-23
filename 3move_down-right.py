dbeb212a0688d241de766b05303284c5b3a8d12a
Ratahama <122821850+Ratahama@users.noreply.github.com>
2023-12-23 13:38:57 +0200

    Add files via upload

--- 3move_down-right.py
+++ 3move_down-right.py
@@ -1,17 +1,38 @@
-def path_counter(row, col, rows: int, cols: int):
-    if row >= rows or col >= cols:
+def path_counter(row: int, col: int, rows: int, cols: int):
+    if row >= rows or col >= cols or row < 0 or col < 0:  # out-of-bounds
         return 0
-    if row == rows-1 and col == cols-1:
+    if row == rows-1 and col == cols-1:  # at exit
         return 1
-
     count = 0
-    count += path_counter(row, col+1, rows, cols)  # Right
-    count += path_counter(row+1, col, rows, cols)  # Down
+    count += path_counter(row+1, col, rows, cols)  # DOWN
+    count += path_counter(row, col+1, rows, cols)  # RIGHT
     return count
 
 
 redove = int(input())
 coloni = int(input())
+# initial position is always 0, 0 topleft cell
+# exit is always at downright-most cell
+# count the amount of paths that can be trodden to the exit
 print(path_counter(0, 0, redove, coloni))
+
+
+
+
+# def path_counter(row, col, rows: int, cols: int):
+#     if row >= rows or col >= cols:
+#         return 0
+#     if row == rows-1 and col == cols-1:
+#         return 1
+#
+#     count = 0
+#     count += path_counter(row, col+1, rows, cols)  # Right
+#     count += path_counter(row+1, col, rows, cols)  # Down
+#     return count
+#
+#
+# redove = int(input())
+# coloni = int(input())
+# print(path_counter(0, 0, redove, coloni))
 
 
--- 4connected_areas_in_matrix.py
+++ 4connected_areas_in_matrix.py
@@ -1,37 +1,33 @@
-def find_all_areas(row, col, rows, cols, matrix):
-    if row < 0 or col < 0 or row >= rows or col >= cols:
+def find_areas(row, col, rows, cols, matrix):
+    if row >= rows or col >= cols or row < 0 or col < 0:  # out-of-bounds
         return 0
     if matrix[row][col] == '*':  # if WALL
         return 0
     if matrix[row][col] == 'v':  # if VISITED
         return 0
-
-    size_area = 1
-    matrix[row][col] = 'v'
-
-    size_area += find_all_areas(row+1, col, rows, cols, matrix)  # DOWN
-    size_area += find_all_areas(row-1, col, rows, cols, matrix)  # UP
-    size_area += find_all_areas(row, col+1, rows, cols, matrix)  # RIGHT
-    size_area += find_all_areas(row, col-1, rows, cols, matrix)  # LEFT
-
-    return size_area
+    matrix[row][col] = 'v'  # marks current cell as visited
+    size_area = 1  # you always start at a beginning cell so that's +1 to size by default
+    size_area += find_areas(row-1, col, rows, cols, matrix)  # UP
+    size_area += find_areas(row+1, col, rows, cols, matrix)  # DOWN
+    size_area += find_areas(row, col+1, rows, cols, matrix)  # RIGHT
+    size_area += find_areas(row, col-1, rows, cols, matrix)  # LEFT
+    return size_area  # this is what gets summed up when backtracking
 
 
-rows = int(input())
-cols = int(input())
-matrix = [[x for x in input()] for _ in range(rows)]
+redove = int(input())
+coloni = int(input())
+matrix = [[x for x in input()] for _ in range(redove)]
 positions = []
-for row in range(rows):  # walkthrough all rows and columns so that you enter their indexes
-    for col in range(cols):  # in the function find_all_areas
-        size = find_all_areas(row, col, rows, cols, matrix)
+for re in range(redove):
+    for co in range(coloni):
+        size = find_areas(re, co, redove, coloni, matrix)
         if size != 0:
-            positions.append((row, col, size))
+            positions.append((re, co, size))
 print(f"Total areas found: {len(positions)}")
 
-sorted_positions = sorted(positions, key=lambda x: -x[2])
-for i in range(len(positions)):
-    print(f"Area #{i+1} at ({sorted_positions[i][0]}, {sorted_positions[i][1]}), size: {sorted_positions[i][2]}")
-
+sorted_positions = sorted(positions, key=lambda x: (-x[2], x[0], x[1]))
+for ai in range(len(sorted_positions)):
+    print(f"Area #{ai+1} at ({sorted_positions[ai][0]}, {sorted_positions[ai][1]}), size: {sorted_positions[ai][2]}")
 
 
 # # INPUT 1:
@@ -50,3 +46,57 @@ for i in range(len(positions)):
 # *--*****--
 # *--*---*--
 # *--*---*--
+
+# # YOU ARE SEARCHING FOR AREAS OF '-' IN A MATRIX:
+# if your matrix is this:
+# ---*---*-
+# ---*---*-
+# ---*---*-
+# ----*-*--
+# the areas are split by walls '*':
+# ---  *  ---  *  -
+# ---  *  ---  *  -
+# ---  *  ---  *  -
+# ----  *  -  *  --
+
+
+
+
+# def find_all_areas(row, col, rows, cols, matrix):
+#     if row < 0 or col < 0 or row >= rows or col >= cols:
+#         return 0
+#     if matrix[row][col] == '*':  # if WALL
+#         return 0
+#     if matrix[row][col] == 'v':  # if VISITED
+#         return 0
+#
+#     size_area = 1
+#     matrix[row][col] = 'v'
+#
+#     size_area += find_all_areas(row+1, col, rows, cols, matrix)  # DOWN
+#     size_area += find_all_areas(row-1, col, rows, cols, matrix)  # UP
+#     size_area += find_all_areas(row, col+1, rows, cols, matrix)  # RIGHT
+#     size_area += find_all_areas(row, col-1, rows, cols, matrix)  # LEFT
+#
+#     return size_area
+#
+#
+# rows = int(input())
+# cols = int(input())
+# matrix = [[x for x in input()] for _ in range(rows)]
+# positions = []
+# for row in range(rows):  # walkthrough all rows and columns so that you use their indexes
+#     for col in range(cols):  # in the function find_all_areas
+#         size = find_all_areas(row, col, rows, cols, matrix)
+#         if size != 0:
+#             positions.append((row, col, size))
+# print(f"Total areas found: {len(positions)}")
+#
+# sorted_positions = sorted(positions, key=lambda x: -x[2])
+# for i in range(len(positions)):
+#     print(f"Area #{i+1} at ({sorted_positions[i][0]}, {sorted_positions[i][1]}), size: {sorted_positions[i][2]}")
+
+
+
+
+
